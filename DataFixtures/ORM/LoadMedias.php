480a4c1c5b35b5516581b2cafd82fdfaf1fb870e
Mykees <mykeespro@gmail.com>
2015-05-29 00:02:34 +0200

    update fixtures and url doc

--- DataFixtures/ORM/LoadMedias.php
+++ DataFixtures/ORM/LoadMedias.php
@@ -19,20 +19,20 @@ class LoadMedias implements FixtureInterface {
         $media = new Media();
         $media->setName('test');
         $media->setFile('2014/11/test.jpg');
-        $media->setModel('Post');
-        $media->setModelId(1);
+        $media->setMediableModel('Post');
+        $media->setMediableId(1);
 
         $media2 = new Media();
         $media2->setName('test2');
         $media2->setFile('2014/11/test2.jpg');
-        $media2->setModel('Post');
-        $media2->setModelId(1);
+        $media2->setMediableModel('Post');
+        $media2->setMediableId(1);
 
         $media3 = new Media();
         $media3->setName('test3');
         $media3->setFile('2014/11/test3.jpg');
-        $media3->setModel('Post');
-        $media3->setModelId(2);
+        $media3->setMediableModel('Post');
+        $media3->setMediableId(2);
 
         $manager->persist($media);
         $manager->flush();
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MkMediaBundle
 The goal of this bundle is to allow you to associate media to yours entities
+
+##Documentation
+[DOC](http://mykees.github.io/MkMediaBundle/)
