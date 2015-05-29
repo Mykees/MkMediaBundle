2c5ca5950549dcd8e1e4fc00ece415c32e63fa73
Mykees <mykeespro@gmail.com>
2015-05-29 13:49:47 +0200

    config travis

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # MkMediaBundle
-The goal of this bundle is to allow you to associate media to yours entities
+The goal of this bundle is to allow you to associate media to your entities
 
 ##Documentation
 [DOC](http://mykees.github.io/MkMediaBundle/)
--- Tests/Controller/MediasControllerTest.php
+++ Tests/Controller/MediasControllerTest.php
@@ -10,81 +10,81 @@ class MediasControllerTest extends WebTestCase
     protected $client;
     protected $container;
     protected $manager;
+    protected $image;
 
     public function setUp()
     {
         //Load fixtures
         $fixtures = array(
             'Mykees\MediaBundle\DataFixtures\ORM\LoadMedias',
-            'Mykees\BlogBundle\DataFixtures\ORM\LoadPosts',
+            'Mvc\BlogBundle\DataFixtures\ORM\LoadPostsData',
         );
         $this->loadFixtures($fixtures);
 
         $this->client = static::createClient();
         $this->container = $this->client->getContainer();
         $this->manager = $this->container->get('mk.media.manager');
+        $this->image = $this->container->get('kernel')->getRootDir().'/../web';
 
         parent::setUp();
     }
 
     public function testListMedias()
     {
-        $crawler = $this->client->request('GET', '/admin/medias/Post/MykeesBlogBundle/1');
+        $crawler = $this->client->request('GET', '/admin/medias/Post/MvcBlogBundle/1');
         $this->assertEquals(200,$this->client->getResponse()->getStatusCode());
         $this->assertEquals(2,$crawler->filter('.count-tr')->count());
 
 
-        $crawler = $this->client->request('GET', '/admin/medias/Post/MykeesBlogBundle/2');
+        $crawler = $this->client->request('GET', '/admin/medias/Post/MvcBlogBundle/2');
         $this->assertEquals(200,$this->client->getResponse()->getStatusCode());
         $this->assertEquals(1,$crawler->filter('.count-tr')->count());
     }
 
     public function testFindAllMediasForAModel()
     {
-        $medias  = $this->manager->findAllMedias('Post',1);
+        $medias  = $this->manager->findMediasByModelAndId('Post',1);
         $this->assertGreaterThan(1,count($medias));
         $this->assertEquals(2, count($medias));
     }
 
     public function testRemoveFile()
     {
-        $this->client->request('GET', '/admin/medias/delete/1');
+        $this->client->request('GET', '/admin/medias/delete/Post/MvcBlogBundle/9');
         $this->assertEquals(200,$this->client->getResponse()->getStatusCode());
 
-        $medias  = $this->manager->findAllMedias('Post',1);
-        $this->assertEquals(1, count($medias));
+        $medias  = $this->manager->findMediasByModelAndId('Post',2);
+        $this->assertEquals(0, count($medias));
     }
 
 
     public function testValidUploadFileFormat()
     {
-        $filePath = dirname(__DIR__).'/../../../../web/images/Elly.jpg';
+        $filePath = dirname(__DIR__).'/../Resources/public/images/elly.jpg';
         $file = new UploadedFile(
             $filePath,
-            'Elly.jpg',
+            'elly.jpg',
             'image/jpeg',
             123
         );
         $this->client->request(
             'POST',
-            '/admin/medias/add/Post/MykeesBlogBundle/2',
+            '/admin/medias/add/Post/MvcBlogBundle/12',
             ['name'=>'Elly'],
             ['file'=>$file],
             ['HTTP_X-Requested-With' => 'XMLHttpRequest']
         );
 
         $this->assertEquals(200,$this->client->getResponse()->getStatusCode());
+        $medias  = $this->manager->findMediasByModelAndId('Post',12);
 
-        $this->manager = $this->container->get('mk.media.manager');
-        $medias  = $this->manager->findAllMedias('Post',2);
-
-        $this->assertEquals(2, count($medias));
+        $this->assertEquals(1, count($medias));
 
     }
 
     public function testInvalidUploadFileFormat()
     {
-        $filePath = dirname(__DIR__).'/../../../../web/images/thumb.png';
+        $filePath = dirname(__DIR__).'/../Resources/public/images/thumb.png';
         $file = new UploadedFile(
             $filePath,
             'thumb.png',
@@ -93,7 +93,7 @@ class MediasControllerTest extends WebTestCase
         );
         $this->client->request(
             'POST',
-            '/admin/medias/add/Post/MykeesBlogBundle/1',
+            '/admin/medias/add/Post/MvcBlogBundle/15',
             ['name'=>'thumb'],
             ['file'=>$file],
             ['HTTP_X-Requested-With' => 'XMLHttpRequest']
@@ -102,10 +102,8 @@ class MediasControllerTest extends WebTestCase
         $this->assertEquals(500,$this->client->getResponse()->getStatusCode());
 
         $this->manager = $this->container->get('mk.media.manager');
-        $medias  = $this->manager->findAllMedias('Post',1);
-        $this->assertEquals(2, count($medias));
+        $medias  = $this->manager->findMediasByModelAndId('Post',15);
+        $this->assertEquals(0, count($medias));
     }
-
-
 
 }
