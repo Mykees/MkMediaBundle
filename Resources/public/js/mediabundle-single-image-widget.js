b25fa3171cdd5dbd7ebcec4640e8f708c4a8d889
Martin Lyne <martin@lynetech.com>
2015-11-21 23:08:23 +0000

    single image widget js and css updates, still incomplete

--- Resources/public/js/mediabundle-single-image-widget.js
+++ Resources/public/js/mediabundle-single-image-widget.js
@@ -1,11 +1,9 @@
 function MediaAjaxImageFetch () {
     $.ajax({
         method: 'post',
-        url: '/media/widget/SiteUser',
-        success: function (a, b) {
-            console.log("success");
-            console.log(a);
-            console.log(b);
+        url: Routing.generate('mykees_media_ajax_fetch_for_model', {model:'SiteUser'}),
+        success: function (response, status) {
+            $('.media-selection-container').html(response);
         }
     });
 }
