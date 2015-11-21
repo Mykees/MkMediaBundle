08bce9c14ab20e0a478453a003af6ad80d0a9eac
Martin Lyne <martin@lynetech.com>
2015-11-21 14:42:32 +0000

    starting work on single image widgets ajax controller/routing

--- Resources/public/js/mediabundle-single-image-widget.js
+++ Resources/public/js/mediabundle-single-image-widget.js
@@ -1,1 +1,15 @@
-console.log("JS here");
+function MediaAjaxImageFetch () {
+    $.ajax({
+        method: 'post',
+        url: '/media/widget/SiteUser',
+        success: function (a, b) {
+            console.log("success");
+            console.log(a);
+            console.log(b);
+        }
+    });
+}
+
+$(function(){
+    MediaAjaxImageFetch();
+});
