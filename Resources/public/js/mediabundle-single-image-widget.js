68a071b738e93dbec209a1aa7a217b9077949feb
Martin Lyne <martin@lynetech.com>
2015-11-23 00:13:01 +0000

    further work on js, css and some minor template tweaks for single image widget

--- Resources/public/js/mediabundle-single-image-widget.js
+++ Resources/public/js/mediabundle-single-image-widget.js
@@ -1,13 +1,75 @@
-function MediaAjaxImageFetch () {
+function MediaAjaxImageFetch (el) {
+    el = $(el);
+    panel = el.closest('.media-widget');
+
     $.ajax({
         method: 'post',
         url: Routing.generate('mykees_media_ajax_fetch_for_model', {model:'SiteUser'}),
         success: function (response, status) {
-            $('.media-selection-container').html(response);
+            el.html(response);
+            PrepareSelectionActions(el, panel);
         }
     });
 }
 
+function PrepareSelectionActions (el, panel) {
+    currentId = $('', panel)
+    $('.media-option', el).each(function(a, mediaItem){
+        mediaItem = $(mediaItem);
+        mediaItem.addClass('clickable');
+        mediaItem.click({clicked: this, context: panel}, ClickMediaOption);
+    });
+}
+
+function ClickMediaOption(e) {
+    // Don't trigger on label propagation
+    if (e.target.tagName.toLowerCase() == 'input') { 
+        clicked = e.data.clicked;
+        context = e.data.context;
+
+        // Add classes
+        $('.media-option', context).removeClass('selected');
+        $(clicked).addClass('selected');
+
+        // Update "Current" and "Previous"
+        UpdateCurrentAndPreviousMedia(clicked, context.parent());
+    }
+}
+
+function UpdateCurrentAndPreviousMedia(clicked, context) {
+    // Play suffle with the values so user knows what status is
+    var previous = $('.previous-image-container', context);
+    var current = $('.current-image-container', context);
+    
+    var previousImg = $('img', previous);
+    var currentImg = $('img', current);
+    var clickedImg = $('img', clicked);
+    
+    var clickedValue = $('input:radio', clicked).val();
+    var originalValue = $('.original-value', context).val();
+    var lastSetValue = $('.last-set-value', context).val();
+
+    var previousName = $('.image-info-text', previous);
+    var currentName = $('.image-info-text', current);
+    var clickedName = $('.filename', clicked);
+
+    if (originalValue == lastSetValue) {
+        previousImg.attr('src', currentImg.attr('src'));
+        previousName.html(currentName.html());
+    }
+    currentName.html(clickedName.html());
+
+    $('.last-set-value', context).val(clickedValue);
+    currentImg.attr('src', clickedImg.attr('src'));
+
+    if (previous.is(':hidden')) {
+        previous.slideDown("slow");
+    }
+
+}
+
 $(function(){
-    MediaAjaxImageFetch();
+    $('.media-selection-container').each(function (a, el) {
+        MediaAjaxImageFetch(el);
+    });
 });
